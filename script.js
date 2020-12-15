2ab04e0199420b23ce92dc2b46e2ae405a393e33
Anirudha sah <anirudha.s@simformsolutions.com>
2020-12-15 23:30:29 +0530

    changes

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// var animation = bodymovin.loadAnimation({
-//     container: document.getElementById('main'),
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true,
-//     path: "bg.json"
-// })
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('main'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "bg.json"
+})
 
 
-// var animation = bodymovin.loadAnimation({
-//     container: document.getElementById('logo'),
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true,
-//     path: "o.json"
-// })
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('logo'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "o.json"
+})
 
 
-// var animation = bodymovin.loadAnimation({
-//     container: document.getElementById('cs'),
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true,
-//     path: "cs.json"
-// })
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('cs'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "cs.json"
+})
